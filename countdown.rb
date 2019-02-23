5e85019ed9ce7fa84fc3008bf20f814b3e73cf4e
Kane Koo <kanekoo@gmail.com>
2019-02-23 20:12:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
 
-def countdown
-  count = 10 
+def countdown (secs)
+  count = secs 
   while (count > 0)
     puts "#{count} SECONDS"
     count -= 1
   end
+  puts "HAPPY NEW YEAR!"
 end
